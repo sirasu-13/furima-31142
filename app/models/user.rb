429f809bfb092075dec28170255766ee17b48503
sirasu-13 <72498856+sirasu-13@users.noreply.github.com>
2020-11-16 18:38:09 +0900

    修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,           presence: true 
-   with_options presence: true, format: { with:/\A[ぁ-んァ-ン一-龥]/ } do
-  validates :last_name
-  validates :first_name
-   end
- with_options presence: true, format: { with:/\A[ァ-ヶー－]+\z/ } do
-  validates :last_name_kana
-  validates :first_name_kana
- end
-  validates :birth_date,         presence: true
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :birth_date, presence: true
 
   has_many :items
   has_many :comments
   has_many :purchases
 end
-
