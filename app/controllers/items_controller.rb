5097df1a1425732d0a3872047c6720f700ee723c
sirasu-13 <72498856+sirasu-13@users.noreply.github.com>
2020-11-19 21:42:05 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item
+  before_action :set_item, only: [:show, :edit, :update ]
   def index
     @items = Item.all.order(created_at: "DESC")
   end
@@ -35,12 +35,13 @@ class ItemsController < ApplicationController
     end
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
-
+  
   private
-
+  
+  def set_item
+  @item = Item.find(params[:id])
+  end
+  
   def item_params
     params.require(:item).permit(:title, :price, :image, :description, :category_id, :status_id, :shipping_fee_id, :area_id, :guideline_id).merge(user_id: current_user.id)
   end
