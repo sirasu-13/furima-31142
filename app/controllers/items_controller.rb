4b88a1c801286e17eee6ea6b98ced310ae43f96f
sirasu-13 <72498856+sirasu-13@users.noreply.github.com>
2020-11-19 22:42:51 +0900

    商品削除機能　修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,7 @@ class ItemsController < ApplicationController
   def destroy
     if user_signed_in? && current_user.id == @item.user_id
       @item.destroy
+      redirect_to root_path
     else
       redirect_to root_path
     end
