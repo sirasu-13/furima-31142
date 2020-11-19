d2ae9cadfcec8323be3a9068cdbfb701dfe485af
sirasu-13 <72498856+sirasu-13@users.noreply.github.com>
2020-11-19 22:09:21 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update, :destroy ]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
-    @items = Item.all.order(created_at: "DESC")
+    @items = Item.all.order(created_at: 'DESC')
   end
 
   def new
@@ -22,34 +22,31 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless user_signed_in? && current_user.id == @item.user_id
-     redirect_to root_path
-    end
+    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
   end
 
   def update
     if @item.update(item_params)
-     redirect_to item_path(@item.id)
-    else 
+      redirect_to item_path(@item.id)
+    else
       render :edit
     end
   end
 
   def destroy
     if user_signed_in? && current_user.id == @item.user_id
-    @item.destroy
-    redirect_to root_path
+      @item.destroy
+    else
+      redirect_to root_path
     end
   end
 
-
-  
   private
-  
+
   def set_item
-  @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
-  
+
   def item_params
     params.require(:item).permit(:title, :price, :image, :description, :category_id, :status_id, :shipping_fee_id, :area_id, :guideline_id).merge(user_id: current_user.id)
   end
